a126bdf10ec7063c2b3b21ef4ff6479d9ee740c5
Nelson Jr <juniordell123@gmail.com>
2020-05-29 18:58:42 -0300

    Últimos ajustes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Email(s) ou matrícula(s): 201072345
 #
 from time import sleep
-
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import random
@@ -108,7 +107,7 @@ for c in range(1, 11):
                 print('=' * 30)
                 print('\t   >> VICTORY!! <<')
                 print('=' * 30)
-                print(f'Sua distância até o alvo foi de {distancia}')
+                print(f'Sua distância até o alvo foi de |{distancia:.2f}|')
                 break
 
         elif escolha == 2:
@@ -116,18 +115,20 @@ for c in range(1, 11):
                 print('=' * 30)
                 print('\t   >> VICTORY!! <<')
                 print('=' * 30)
-                print(f'Sua distância até o alvo foi de {distancia}')
+                print(f'Sua distância até o alvo foi de |{distancia:.2f}|')
                 break
 
     elif escolha == 3:
         plt.plot(listax, listay)
         plt.show()
+
         if distancia < 100:
-            print(f'Bahh!! Chegou perto! Sua distância é de {distancia} até o alvo.')
+            print(f'Bahh!! Chegou perto! Sua distância é de {distancia:.2f} até o alvo.')
         elif distancia < 300:
-            print(f'Ainda pode melhorar! Sua distância é de {distancia} até o alvo.')
+            print(f'Ainda pode melhorar! Sua distância é de {distancia:.2f} até o alvo.')
         else:
-            print(f'Ta muito frio ainda, mas tem chance! Sua distância é de {distancia} até o alvo.')
+            print(f'Ta muito frio ainda, mas tem chance! Sua distância é de {distancia:.2f} até o alvo.')
+
         if distancia < 2:
             plt.plot(posx, 10, 'ro')
             plt.show()
@@ -135,11 +136,12 @@ for c in range(1, 11):
             print('\t   >> VICTORY!! <<')
             print('=' * 30)
             print('Uauu! Você conseguiu no modo difícil, parabéns!!')
-            print(f'Sua distância até o alvo foi de {distancia}')
+            print(f'Sua distância até o alvo foi de |{distancia:.2f}|')
             break
     else:
         plt.plot(posx, 10, 'ro')
         plt.show()
+
         if distancia > 500:
             print('A sua distância até o alvo é maior que 500')
         elif distancia > 300:
@@ -149,17 +151,16 @@ for c in range(1, 11):
         elif distancia < 100:
             print('A sua distância até o alvo é menor que 100')
         if c == 5:
-            print(f'Sua distância até o alvo foi de {distancia}')
+            print(f'Sua distância até o alvo foi de |{distancia:.2f}|')
             break
-        if distancia < 1:
-            print('USAR HACK NÃO É PERMITIDO!')
-# FIM DA REPETIÇÃO DE TENTATIVAS
 
-# AO FINAL, fazer close() para fechar a janela
+        if distancia < 1:
+            print('\033[31mUSAR HACK NÃO É PERMITIDO!\033[m')
+
 input("ENTER para finalizar")
 print()
 print('Finalizando...')
 sleep(3)
 print()
-print('\t\t|Obrigado por jogar, volte sempre!|')
+print('**Obrigado por jogar, volte sempre!**')
 plt.close()
